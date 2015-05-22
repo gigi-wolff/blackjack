127041facbf9dcfffc9781e011508ce7eb2c48b9
Gigi Wolff <gigi.wolff@gmail.com>
2015-05-22 16:57:39 -0400

    modify based on code review comments

--- blackjack_multi.rb
+++ blackjack_multi.rb
@@ -3,7 +3,6 @@ SUITS = ['Hearts', 'Spades', 'Clubs','Diamonds']
 CARDS = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
 FACE_CARD = ['Jack','Queen','King','Ace']
 
-
 def hand_total(hand)
   total = 0
   hand.each do |card|
@@ -32,7 +31,7 @@ def hit_or_stay(name)
   begin
     puts name + " ==> do you want to (H)it or (S)tay?"
     answer = gets.chomp.downcase
-  end until (answer=='h' || answer=='s') 
+  end until (['h','s'].include?(answer)) 
   answer 
 end
 
@@ -40,13 +39,14 @@ def get_player_num
   begin
     puts "How many people want to play? (max 5)"
     answer = gets.chomp
-  end until (answer.to_i <= 5 && answer.to_i > 0) 
+  end until ((1..5).include?(answer.to_i)) 
   answer.to_i 
 end
 
 def play_a_hand(name,deck)
   player_hand = []
   player_total = 0
+  player_hand << deck.pop
   # deal player's hand until he 'stays', reaches 21 or busts
   while (player_total<21)
     # deal player a card
@@ -56,8 +56,8 @@ def play_a_hand(name,deck)
     player_total = hand_total(player_hand)
 
     # display hand and total
-    puts name + "'s Hand: "+ display_hand(player_hand)
-    puts "    TOTAL: " + player_total.to_s
+    puts "#{name}'s Hand: #{display_hand(player_hand)}"
+    puts "    TOTAL: #{player_total.to_s}"
 
     # ask if player wants another card if hand is less than 21
     if player_total<21
@@ -81,18 +81,18 @@ def get_players
   players_hands
 end
 
-#------------------- Main -------------------
-#initializatons
+def remove_player_from_game(player_name,players)
+  players.delete(player_name)
+end
+
 players = {} #player = {name => hand value}
 deck = CARDS.product(SUITS)
-deck = (deck + deck + deck + deck).shuffle! #play with four decks
+deck = (deck*4).shuffle! #play with four decks
 dealer_hand = []
 dealer_total = 0
 
-#players in the game
 players = get_players
-
-players.each {|name,v| puts "Welcome #{name}!"}
+players.each {|name,_v| puts "Welcome #{name}!"}
 
 # play a hand for each player in the hand
 players.each do |name,hand_value|
@@ -101,7 +101,7 @@ players.each do |name,hand_value|
   # Display messages, remove player from game if busted
   if (players[name] > 21) then
     puts "Busted!"
-    players.delete(name) # player is out of bame
+    remove_player_from_game(name,players)
   elsif (players[name] == 21) #blackjack, no more cards needed
     puts "Blackjack!"
   end
@@ -111,6 +111,7 @@ end
 exit if players.empty?
 
 # deal computer's a card until he reaches 17 or busts
+dealer_hand << deck.pop
 while (dealer_total < 17)
   # deal a card to dealer
   dealer_hand << deck.pop
@@ -119,8 +120,8 @@ while (dealer_total < 17)
   dealer_total = hand_total(dealer_hand)
 
   #display hand and total
-  puts "Dealer's Hand: "+display_hand(dealer_hand)
-  puts "        TOTAL: " + dealer_total.to_s
+  puts "Dealer's Hand:  #{display_hand(dealer_hand)}"
+  puts "        TOTAL:  #{dealer_total.to_s}"
 end
 
 # if neither dealer nor player has busted, whomever is closest to 21 wins
@@ -129,9 +130,8 @@ players.each do |name,hand_total|
     puts "Blackjack! Dealer wins."
     exit
   elsif (dealer_total > hand_total && dealer_total <21)
-    #puts "Dealer wins."
-    puts "Sorry, "+name+". You lose."
+    puts "Sorry, #{name}. You lose."
   else
-    puts name + " wins!"
+    puts "#{name} wins!"
   end
 end
