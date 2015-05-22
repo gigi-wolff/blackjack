b749c7791e9c8e88d5a5a7e025b15575db19f27a
Gigi Wolff <gigi.wolff@gmail.com>
2015-05-21 21:36:51 -0400

    refactored and fixed indentation

--- blackjack_multi.rb
+++ blackjack_multi.rb
@@ -12,28 +12,36 @@ def hand_total(hand)
         total += 11
       else
         total += 10
-      end 
+      end
     else
       total += card[0].to_i
     end
   end
-  # Set value of Ace in hand to 1 if previous value of 11 causes bust  
-  total -=10 if (total>21 && hand.flatten.include?('Ace')) 
-  return total
+  # Set value of Ace in hand to 1 if previous value of 11 causes bust
+  total -=10 if (total>21 && hand.flatten.include?('Ace'))
+  total
 end
 
 def display_hand(hand)
   current_hand = ""
   hand.each {|card| current_hand += "|#{card[0]} of #{card[1]}|  "}
-  return current_hand
-end   
+  current_hand
+end
 
-def get_input(name)
+def hit_or_stay(name)
   begin
-    puts name + ", do you want to (H)it or (S)tay?"
-    answer = gets.chomp.downcase  
-  end until (answer=='h' || answer=='s')
-  return answer
+    puts name + " ==> do you want to (H)it or (S)tay?"
+    answer = gets.chomp.downcase
+  end until (answer=='h' || answer=='s') 
+  answer 
+end
+
+def get_player_num
+  begin
+    puts "How many people want to play? (max 5)"
+    answer = gets.chomp
+  end until (answer.to_i <= 5 && answer.to_i > 0) 
+  answer.to_i 
 end
 
 def play_a_hand(name,deck)
@@ -42,95 +50,88 @@ def play_a_hand(name,deck)
   # deal player's hand until he 'stays', reaches 21 or busts
   while (player_total<21)
     # deal player a card
-    player_hand << deck.pop 
+    player_hand << deck.pop
 
     #  find total value of hand
     player_total = hand_total(player_hand)
 
     # display hand and total
-    puts name + "'s Hand: "+ display_hand(player_hand) 
+    puts name + "'s Hand: "+ display_hand(player_hand)
     puts "    TOTAL: " + player_total.to_s
 
     # ask if player wants another card if hand is less than 21
     if player_total<21
-      player_response = get_input(name)
+      player_response = hit_or_stay(name)
       break if player_response=='s'
     end
   end
-  return player_total
+  player_total
 end
 
 def get_players
-  players_hands = {} 
-  puts "Welcome to 4-deck Blackjack. How many people want to play?"
-  num_of_players = gets.chomp
-  num_players = num_of_players.to_i
-
+  players_hands = {}
+  puts "Welcome to 4-deck Blackjack."
+  num_players = get_player_num
+  
   num_players.times do |num|
     puts "Please enter name of #{num+1}:"
     name = gets.chomp
     players_hands[name] = 0
   end
-  return players_hands
+  players_hands
 end
 
 #------------------- Main -------------------
 #initializatons
-player = {} #player = {name => hand value}
+players = {} #player = {name => hand value}
 deck = CARDS.product(SUITS)
 deck = (deck + deck + deck + deck).shuffle! #play with four decks
 dealer_hand = []
 dealer_total = 0
 
 #players in the game
-player = get_players
+players = get_players
 
-player.each {|name,v| puts "Welcome #{name}!"}
+players.each {|name,v| puts "Welcome #{name}!"}
 
 # play a hand for each player in the hand
-player.each do |name,hand_value|  
-  player[name] = play_a_hand(name,deck)
+players.each do |name,hand_value|
+  players[name] = play_a_hand(name,deck)
 
   # Display messages, remove player from game if busted
-  if (player[name] > 21) then
+  if (players[name] > 21) then
     puts "Busted!"
-    player.delete(name) # player is out of bame
-  elsif (player[name] == 21) #blackjack, no more cards needed
-     puts "Blackjack!"
+    players.delete(name) # player is out of bame
+  elsif (players[name] == 21) #blackjack, no more cards needed
+    puts "Blackjack!"
   end
 end
 
 #Quit game if no players are left
-exit if player.empty?
+exit if players.empty?
 
 # deal computer's a card until he reaches 17 or busts
 while (dealer_total < 17)
-    # deal a card to dealer 
-    dealer_hand << deck.pop
-    
-    # find total value of hand
-    dealer_total = hand_total(dealer_hand)
+  # deal a card to dealer
+  dealer_hand << deck.pop
 
-    #display hand and total
-    puts "Dealer's Hand: "+display_hand(dealer_hand)
-    puts "        TOTAL: " + dealer_total.to_s  
-end
+  # find total value of hand
+  dealer_total = hand_total(dealer_hand)
 
-# Display messages, end game if dealer busted
-if (dealer_total > 21) then
-  puts "Dealer busted. " + name + " Wins! Game Over."    
-  exit
-elsif (dealer_total==21) #blackjack, break out 
-  puts "Blackjack! Dealer wins."
-  exit       
+  #display hand and total
+  puts "Dealer's Hand: "+display_hand(dealer_hand)
+  puts "        TOTAL: " + dealer_total.to_s
 end
 
 # if neither dealer nor player has busted, whomever is closest to 21 wins
-player.each do |name,hand_total|
-  if (dealer_total > hand_total)
-    puts "Dealer wins."
+players.each do |name,hand_total|
+  if (dealer_total == 21) then
+    puts "Blackjack! Dealer wins."
+    exit
+  elsif (dealer_total > hand_total && dealer_total <21)
+    #puts "Dealer wins."
+    puts "Sorry, "+name+". You lose."
   else
-   puts name + " wins!"
+    puts name + " wins!"
   end
 end
-
--- blackjack_single.rb
+++ blackjack_single.rb
@@ -11,28 +11,28 @@ def hand_total(hand)
         total += 11
       else
         total += 10
-      end 
+      end
     else
       total += card[0].to_i
     end
   end
-  # Set value of Ace in hand to 1 if previous value of 11 causes bust  
-  total -=10 if (total>21 && hand.flatten.include?('Ace')) 
-  return total
+  # Set value of Ace in hand to 1 if previous value of 11 causes bust
+  total -=10 if (total>21 && hand.flatten.include?('Ace'))
+  total
 end
 
 def display_hand(hand)
   current_hand = ""
   hand.each {|card| current_hand += "|#{card[0]} of #{card[1]}|  "}
-  return current_hand
-end   
+  current_hand
+end
 
 def get_input
   begin
     puts "(H)it or (S)tay"
-    answer = gets.chomp.downcase  
-  end until (answer=='h' || answer=='s')
-  return answer
+    answer = gets.chomp.downcase
+  end until (answer=='h' || answer=='s') 
+  answer 
 end
 #------------------- Main -------------------
 deck = CARDS.product(SUITS)
@@ -51,13 +51,13 @@ player_total = 0
 # deal player's hand until he 'stays', reaches 21 or busts
 begin
   # deal player a card
-  player_hand << deck.pop 
+  player_hand << deck.pop
 
   #  find total value of hand
   player_total = hand_total(player_hand)
 
   # display hand and total
-  puts "Your Hand: "+ display_hand(player_hand) 
+  puts "Your Hand: "+ display_hand(player_hand)
   puts "    TOTAL: " + player_total.to_s
 
   # if player busts, quit program
@@ -66,7 +66,7 @@ begin
     puts "Game Over"
     exit
   elsif (player_total == 21) #blackjack, no more cards needed
-    break        
+    break
   else # ask if player wants another card
     response = get_input
   end
@@ -74,8 +74,8 @@ end until (response=='s')
 
 
 # deal computer's a card until he reaches 17 or busts
-begin 
-  # deal a card to dealer 
+begin
+  # deal a card to dealer
   dealer_hand << deck.pop
 
   # find total value of hand
@@ -83,21 +83,21 @@ begin
 
   #display hand and total
   puts "Dealer's Hand: "+display_hand(dealer_hand)
-  puts "        TOTAL: " + dealer_total.to_s  
+  puts "        TOTAL: " + dealer_total.to_s
 
   #if dealer busted display message and quit program
   if (dealer_total > 21) then
     puts "Dealer busted. You Win!"
-    puts "Game Over" 
+    puts "Game Over"
     exit
-  elsif dealer_total==21 #blackjack, break out 
-    break        
+  elsif dealer_total==21 #blackjack, break out
+    break
   end
 end until (dealer_total>=17)
 
 # if neither dealer nor player has busted, whomever is closest to 21 wins
 if (dealer_total > player_total)
-   puts "Dealer wins."
+  puts "Dealer wins."
 elsif (dealer_total == player_total)
   puts "Its a tie. Dealer wins."
 else
